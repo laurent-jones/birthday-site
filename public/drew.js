6bde8e5e4a80c5dd778f3fd21d59f8c52c4b6692
Drew McMillan <drew.mcmillan@thetrainline.com>
2020-04-16 14:56:56 +0100

    clean up

--- public/drew.js
+++ public/drew.js
@@ -1,25 +1,21 @@
-if (window.location.href.includes("drew")) {
-  const getRandomColor = () => {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
+const getRandomColor = () => {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
-  const magic = () => {
-    Array(...document.getElementsByClassName("letter")).forEach((el) =>
-      el.setAttribute("style", `color: ${getRandomColor()};`)
-    );
-    document
-      .getElementsByTagName("body")[0]
-      .setAttribute("style", `background-color: ${getRandomColor()};`);
-  };
+const magic = () => {
+  Array(...document.getElementsByClassName("letter")).forEach((el) =>
+    el.setAttribute("style", `color: ${getRandomColor()};`)
+  );
+  document
+    .getElementsByTagName("body")[0]
+    .setAttribute("style", `background-color: ${getRandomColor()};`);
+};
 
-  magic();
+magic();
 
-  window.setInterval(magic, 800);
-}
-
-console.log("HEYYY");
+window.setInterval(magic, 800);
